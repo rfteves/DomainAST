5112ade74b45109c3317553e08c2171e8f0bede9
Ricardo Teves <rfteves@gmail.com>
2018-06-25 18:27:29 -0700

    Initial commit

--- src/main/java/doomast/DomainTransformation.java
+++ src/main/java/doomast/DomainTransformation.java
@@ -1,9 +1,6 @@
 package doomast;
 
-import org.codehaus.groovy.ast.ASTNode;
-import org.codehaus.groovy.ast.ClassHelper;
-import org.codehaus.groovy.ast.ClassNode;
-import org.codehaus.groovy.ast.Parameter;
+import org.codehaus.groovy.ast.*;
 import org.codehaus.groovy.ast.expr.*;
 import org.codehaus.groovy.ast.stmt.*;
 import org.codehaus.groovy.ast.tools.GeneralUtils;
@@ -11,21 +8,27 @@ import org.codehaus.groovy.control.CompilePhase;
 import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.transform.AbstractASTTransformation;
 import org.codehaus.groovy.transform.GroovyASTTransformation;
+
 import java.lang.reflect.Modifier;
 import java.net.URI;
+import java.util.Arrays;
 
-@GroovyASTTransformation(phase = CompilePhase.SEMANTIC_ANALYSIS)
+@GroovyASTTransformation(phase = CompilePhase.INSTRUCTION_SELECTION)
 public class DomainTransformation extends AbstractASTTransformation {
 
     @Override
     public void visit(ASTNode[] nodes, SourceUnit sourceUnit) {
         if (IsValid(sourceUnit)) {
+            addInnerClass(sourceUnit);
             sourceUnit.getAST().getClasses().stream()
-                    .forEach(classNode -> visit(classNode));
+                    .forEach(classNode -> {
+                        visit(classNode);
+                        displayInterfaces(classNode);
+                    });
         }
     }
 
-    private boolean IsValid(SourceUnit sourceUnit) {
+    static private boolean IsValid(SourceUnit sourceUnit) {
 
         URI sourceURI = sourceUnit.getSource().getURI();
 
@@ -35,14 +38,14 @@ public class DomainTransformation extends AbstractASTTransformation {
         return true;
     }
 
-    private boolean IsValidURIPath(String path) {
+    static private boolean IsValidURIPath(String path) {
         if (path == null)
             return false;
 
         if (!path.endsWith(".groovy"))
             return false;
 
-        if (!path.contains("domain/"))
+        if (!path.contains("domain/pr/Account"))
             return false;
 
         System.out.println("path is " + path);
@@ -58,7 +61,7 @@ public class DomainTransformation extends AbstractASTTransformation {
             record
         }
     */
-    private void visit(ClassNode classNode) {
+    static private void visit(ClassNode classNode) {
 
         Parameter idParameter = new Parameter(
                 ClassHelper.make(Long.class),
@@ -74,7 +77,6 @@ public class DomainTransformation extends AbstractASTTransformation {
         methodBlockStatement.addStatement(new ExpressionStatement(recordVariableExpression));
 
 
-
         classNode.addMethod(
                 "getById",
                 Modifier.STATIC,
@@ -88,7 +90,7 @@ public class DomainTransformation extends AbstractASTTransformation {
     /*
         def record = Domain.read(id)
      */
-    private Statement RecordReadStatement(ClassNode domainClassNode, VariableExpression recordVariableExpression, Expression idExpression) {
+    static private Statement RecordReadStatement(ClassNode domainClassNode, VariableExpression recordVariableExpression, Expression idExpression) {
 
         MethodCallExpression domainRead = new MethodCallExpression(
                 new ClassExpression(domainClassNode),
@@ -109,7 +111,7 @@ public class DomainTransformation extends AbstractASTTransformation {
             throw new RecordNotFoundException("Domain, "read", id)
         }
     */
-    private IfStatement IfNotRecordStatement(ClassNode domainClassNode, Expression recordExpression, Expression idExpression) {
+    static private IfStatement IfNotRecordStatement(ClassNode domainClassNode, Expression recordExpression, Expression idExpression) {
 
         BooleanExpression notRecord = new BooleanExpression(
                 new NotExpression(recordExpression)
@@ -128,7 +130,7 @@ public class DomainTransformation extends AbstractASTTransformation {
     /*
         throw new RecordNotFoundException("Domain", "read", id)
      */
-    private ThrowStatement ThrowRecordNotFoundStatement(ClassNode domainClassNode, Expression idExpression) {
+    static private ThrowStatement ThrowRecordNotFoundStatement(ClassNode domainClassNode, Expression idExpression) {
 
         ArgumentListExpression exceptionArguments = new ArgumentListExpression(
                 new ConstantExpression(domainClassNode.getNameWithoutPackage()),
@@ -137,10 +139,84 @@ public class DomainTransformation extends AbstractASTTransformation {
         );
 
         ConstructorCallExpression newRecordNotFoundException = new ConstructorCallExpression(
-                ClassHelper.make("com.gotkcups.exception.RecordNotFoundException"),
+                ClassHelper.make("pr.exception.RecordNotFoundException"),
                 exceptionArguments
         );
 
         return new ThrowStatement(newRecordNotFoundException);
+    }
+
+    /**
+     * class DateRange implements Validateable {
+     * <p>
+     * // Properties
+     * Date startDate = Date.valueOf(LocalDate.now())
+     * Date endDate
+     * <p>
+     * static constraints = {
+     * endDate:
+     * NULLABLE
+     * }
+     * }
+     *
+     * @param sourceUnit
+     */
+    private void addInnerClass(SourceUnit sourceUnit) {
+        ClassNode implementsValidateable = new ClassNode(
+                "grails.validation.Validateable",
+                (Modifier.ABSTRACT + Modifier.INTERFACE + Modifier.PUBLIC),
+                null
+        );
+        InnerClassNode innerClassNode = new InnerClassNode(
+                new ClassNode(java.lang.Object.class),
+                "pr.Account$DateRange",
+                Modifier.STATIC,
+                new ClassNode(java.lang.Object.class),
+                new ClassNode[]{implementsValidateable},
+                null
+        );
+
+        ArgumentListExpression arguments = new ArgumentListExpression();
+        arguments.addExpression(new ClassExpression(new ClassNode(java.time.LocalDate.class)));
+        arguments.addExpression(new ConstantExpression("now"));
+
+        MethodCallExpression methodCallExpression = new MethodCallExpression(
+                new ClassExpression(new ClassNode(java.sql.Date.class)),
+                new ConstantExpression("valueOf"),
+                arguments
+        );
+
+        FieldNode fieldNode = new FieldNode(
+                "startDate",
+                Modifier.PRIVATE,
+                new ClassNode(java.sql.Date.class),
+                innerClassNode,
+                methodCallExpression
+        );
+
+        PropertyNode propertyNode = new PropertyNode(fieldNode, Modifier.PUBLIC, null, null);
+
+        innerClassNode.getProperties().add(propertyNode);
+
+        sourceUnit.getAST().addClass(innerClassNode);
+
+    }
+
+    private void displayInterfaces(ClassNode classNode) {
+        if (classNode.getName().contains("Gone")) {
+            classNode.getProperties().stream().forEach(propertyNode ->
+                    {
+
+                        FieldNode fieldNode = propertyNode.getField();
+                        System.out.println("fieldNode " + fieldNode.getName());
+                        System.out.println("propertyNode " + propertyNode.getName());
+                        System.out.println("fieldNode-modifiers " + fieldNode.getModifiers());
+                        System.out.println("propertyNode-modifiers " + propertyNode.getModifiers());
+                        System.out.println("propertyNode-getter-block " + propertyNode.getGetterBlock());
+                        System.out.println("propertyNode-setter-block " + propertyNode.getSetterBlock());
+                        System.out.println("propertyNode-setter-block " + propertyNode.getSetterBlock());
+                    }
+            );
+        }
     }
 }
